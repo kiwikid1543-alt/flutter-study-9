4a9c078ff968d440057583107051758094fa33d4
kiwikid1543-alt <kiwikid1543@gmail.com>
2025-12-17 09:54:47 +0900

    feat: 네임드 생성자

--- code/d251217_2.dart
+++ code/d251217_2.dart
@@ -1,12 +1,19 @@
 void main() {
   //
   print("프로그램 실행됨");
-  Human h1 = Human('강훈', age: 20, gender: "Male", haircolor: "Black");
-  Human h2 = Human(
-    '이지',
+  // Human h1 = Human(
+  //   '강훈',
+  //   age: 20,
+  //   gender: "Male",
+  //   haircolor: "Black",
+  //   skinColor: "WHITE",
+  // );
+  Human h1 = Human.whiteSkin('강성훈', age: 20, gender: "male");
+  Human h2 = Human.blackSkin(
+    '이지원',
     age: 20,
-    gender: "Male",
-    child: Human("상구", age: 9, gender: "Male"),
+    gender: "male",
+    child: Human.yellowSkin("상구", age: 9, gender: "male"),
   );
 }
 
@@ -21,11 +28,38 @@ class Human {
   String gender;
   String? haircolor;
   Human? child;
+  String skinColor;
   Human(
     this.name, {
     required this.age,
     required this.gender,
     this.haircolor,
     this.child,
+    required this.skinColor,
   });
+  // 네임드 생성자
+  // 객체를 생성하면서 특정한 값을 할당하거나 특정한 기능을 수행하게 하고 싶을 때 사용
+  // 특정한 역할을 한다는걸 객체 생성할때 알기 쉽게 하기위해서
+  // 생성자에 이름을 붙일 수 있음!
+  Human.blackSkin(
+    this.name, {
+    required this.age,
+    required this.gender,
+    this.haircolor,
+    this.child,
+  }) : this.skinColor = "Black";
+  Human.whiteSkin(
+    this.name, {
+    required this.age,
+    required this.gender,
+    this.haircolor,
+    this.child,
+  }) : this.skinColor = "White";
+  Human.yellowSkin(
+    this.name, {
+    required this.age,
+    required this.gender,
+    this.haircolor,
+    this.child,
+  }) : this.skinColor = "Yellow";
 }
